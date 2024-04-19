1b3363ec00969bae7826655799253b3a712ab0b7
He Zhu <hzhu2202@uni.sydney.edu.au>
2024-04-19 15:57:07 +1000

    fix: improve the UI

--- lib/components/post_table.dart
+++ lib/components/post_table.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:forum_app/models/post_model.dart';
 import 'package:forum_app/state/post.dart';
 import "package:provider/provider.dart";
+import 'post_page.dart';
 
 String formatElapsedTime(DateTime postTime) {
   final Duration difference = DateTime.now().difference(postTime);
@@ -24,47 +28,81 @@ class PostsTable extends StatelessWidget {
       return Center(child: CircularProgressIndicator());
     }
 
-    return ListView.builder(
-        itemCount: postsModel.posts.length,
+    return ListView.separated(
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemCount: postsModel.posts.length +
+            (postsModel.currentPage <= postsModel.totalPages ? 1 : 0),
         itemBuilder: (context, index) {
+          if (index == postsModel.posts.length) {
+            return Center(child: CircularProgressIndicator());
+          }
           final post = postsModel.posts[index];
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(post.avatarUrl),
-                        onBackgroundImageError: (_, __) => Icon(Icons.person),
-                      ),
-                      SizedBox(width: 8),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(post.author,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(formatElapsedTime(
-                                DateTime.parse(post.createdAt))),
-                          ],
-                        ),
-                      ),
-                      Text('${post.commentsCount} replies',
-                          style: TextStyle(color: Colors.grey[600])),
-                    ],
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PostPage(
+                    postId: post.id!,
                   ),
-                  SizedBox(height: 8),
-                  Text(post.title,
-                      style: Theme.of(context).textTheme.headline6),
-                  SizedBox(height: 4),
-                  Text(post.content),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
+            child: postItem(context, post),
           );
         });
+  }
+
+  Widget postItem(BuildContext context, Post post) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(children: [
+            Icon(
+              Icons.message,
+              size: 16,
+              color: Colors.grey,
+            ),
+            Text('${post.commentsCount} ',
+                style: TextStyle(color: Colors.grey[600])),
+          ]),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(post.title,
+                    style: Theme.of(context).textTheme.titleMedium),
+              ),
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: post.author.avatarUrl != null
+                        ? NetworkImage(post.author.avatarUrl!)
+                        : AssetImage('lib/assets/defaultAvatar.png')
+                            as ImageProvider,
+                    onBackgroundImageError: (_, __) => Icon(Icons.person),
+                    radius: 10,
+                  ),
+                  SizedBox(width: 8),
+                  Text(post.author.username!,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey)),
+                  SizedBox(width: 8),
+                  Text(
+                    formatElapsedTime(DateTime.parse(post.createdAt)),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
